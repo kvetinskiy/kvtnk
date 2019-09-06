ef74084384764f1f6ef2baa96e7ecdb1dbc6f6b1
kvetinskiy <nikenike932@gmail.com>
2019-09-06 22:21:46 +0600

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const TelegramBot = require('node-telegram-bot-api');
+const telegraf = require('988296646:AAEt3dX5t5Sodf8z2sKm2-MNciJJg2X0D-A');
 
-const bot = new TelegramBot(process.env.BOT_TOKEN)
+const bot = new telegraf(process.env.988296646:AAEt3dX5t5Sodf8z2sKm2-MNciJJg2X0D-A)
 bot.start((ctx) => ctx.reply('Здравствуйте! Мы занимаемся созданием и администрированием ботов телеграм. Для просмотра меню нажмите /help'))
 bot.help((ctx) => ctx.reply('Для просмотра продуктов нажмите /shop . Для получения информации о нас нажмите /info . Для просмотра вводного видео нажмите /video . '))
 bot.shop((ctx) => ctx.reply('Продукт 1 - 120. Продукт 2 - 982. Продукт 3 - ...'))
