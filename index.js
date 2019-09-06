68f3eb61a50e8a5eef5ca5abf6834d4135b7b862
kvetinskiy <nikenike932@gmail.com>
2019-09-06 23:32:32 +0600

    index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const TelegramBot = require('node-telegram-bot-api');
-
+const request = require("request");
 // replace the value below with the Telegram token you receive from @BotFather
 const token = '988296646:AAEt3dX5t5Sodf8z2sKm2-MNciJJg2X0D-A';
 
@@ -7,7 +7,7 @@ const token = '988296646:AAEt3dX5t5Sodf8z2sKm2-MNciJJg2X0D-A';
 const bot = new TelegramBot(token, {polling: true});
 
 // Matches "/echo [whatever]"
-bot.onText(/\/products (.+)/, (msg, match) => {
+bot.onText(/\/menu (.+)/, (msg, match) => {
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
   // of the message
@@ -16,31 +16,22 @@ bot.onText(/\/products (.+)/, (msg, match) => {
   const resp = match[1]; // the captured "whatever"
 
   // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, "Товар  - 300");
-});
-
-// Matches "/echo [whatever]"
-bot.onText(/\/buis (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, "Что-то про бизнес");
-});
-
-// Matches "/echo [whatever]"
-bot.onText(/\/contacts (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, "Тут связь с нами");
+  bot.sendMessage(chatId, "Что вас интересует?", {
+    reply_markup: {
+      inline_keyboard: [
+          [
+            {
+              text: "Продукты"
+              callback_data: "Продукты"
+            }, {
+              text: "Бизнес"
+              callback_data: "Бизнес"
+            }, {
+              text: "Обратная связь"
+              callback_data: "Обратная связь"
+            }
+          ]
+      ]
+}
+};
 });
