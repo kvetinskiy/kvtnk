c1fdb41d8feae0dd8c4a9e1934bdcee8237ae194
kvetinskiy <nikenike932@gmail.com>
2019-09-06 22:52:59 +0600

    index.js

--- index.js
+++ index.js
@@ -1,8 +1,46 @@
-const Telegraf = require('telegraf')
+const TelegramBot = require('node-telegram-bot-api');
 
-const bot = new Telegraf(process.env.988296646:AAEt3dX5t5Sodf8z2sKm2-MNciJJg2X0D-A)
-bot.start((ctx) => ctx.reply('Welcome!'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply(''))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.launch()
+// replace the value below with the Telegram token you receive from @BotFather
+const token = '988296646:AAEt3dX5t5Sodf8z2sKm2-MNciJJg2X0D-A';
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+// Matches "/echo [whatever]"
+bot.onText(/\/products (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, "Товар  - 300");
+});
+
+// Matches "/echo [whatever]"
+bot.onText(/\/buis (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, "Что-то про бизнес");
+});
+
+// Matches "/echo [whatever]"
+bot.onText(/\/contacts (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, "Тут связь с нами");
+});
